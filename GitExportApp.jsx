f926e52ebc46820fa2b8d90c5dbece6461931358
adrian Estrada <aestradacolina@gmail.com>
2023-04-19 17:49:03 -0400

    first commit

---
+++ GitExportApp.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+export const GitExpertApp = () => {
+  return (
+    <>
+      <h1>GitExpertApp</h1>{" "}
+    </>
+  );
+};
---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import { GitExportApp } from "./GitExportApp";
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StricMode>
+    <GitExportApp />
+  </React.StricMode>
+);
