48bb254bfdef96d27d9135293366b1c4fd9d1f73
edgarolivar16 <edgarolivar16@gmail.com>
2023-04-19 21:14:21 -0400

    arreglo de los archivos

--- GitExportApp.jsx
+++
@@ -1,9 +0,0 @@
-import react from "react";
-
-export const GitExpertApp = () => {
-  return (
-    <>
-      <h1>GitExpertApp</h1>{" "}
-    </>
-  );
-};
--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-import { GitExportApp } from "./GitExportApp";
-
-ReactDom.createRoot(document.getElementById("root")).render(
-  <React.StricMode>
-    <GitExportApp />
-  </React.StricMode>
-);
