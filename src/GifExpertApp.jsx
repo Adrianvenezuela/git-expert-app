0bd8262f718320a5c0eee1c9415986f5c07b7d24
adrian Estrada <aestradacolina@gmail.com>
2023-04-26 09:29:27 -0400

    commit

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
-  return (
-    <div>GifExpertApp</div>
-  )
-}
+  const [categories, setCategories] = useState(["One punch", "Dragon Boll"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    //categories.push(newCategory);
+    setCategories([newCategory, ...categories]);
+  };
+};
+<>
+  {/* titulo */}
+  <div>GifExpertApp</div>
+
+  {/* input */}
+  <AddCategory
+    //setCategories={ setCategories }
+    onNewCategory={ (value) => onAddCategory(value) }
+  />
+
+  {/* listado de Gif */}
+
+  <ol>
+    {categories.map((category) => (
+      <div key={category}>
+        <h3>{category}</h3>
+        <li>{category}</li>
+      </div>
+    ))}
+  </ol>
+
+  {/* Git Item */}
+</>;
